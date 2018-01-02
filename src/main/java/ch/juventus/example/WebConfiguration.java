4729e180c8477a80c04909fb91e1efba31e5fcff
Sebastian Haehnel <sebastian.haehnel@gmail.com>
2018-01-02 22:33:47 +0100

    initial

---
+++ src/main/java/ch/juventus/example/WebConfiguration.java
@@ -0,0 +1,38 @@
+package ch.juventus.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = WebConfiguration.class)
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+     //   registry.enableContentNegotiation(new MappingJackson2JsonView());
+        registry.freeMarker().cache(false);
+        registry.freeMarker().suffix(".html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources");
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/WEB-INF/views");
+        return configurer;
+    }
+
+}
